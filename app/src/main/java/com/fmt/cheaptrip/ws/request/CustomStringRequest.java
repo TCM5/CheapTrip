cedb0e9ffeb46f4e8e6e8d9dd45ff048d95500f0
Tiago Santos <tcmsbd@gmail.com>
2016-05-25 17:39:57 +0100

    cleaning comments

--- app/src/main/java/com/fmt/cheaptrip/ws/request/CustomStringRequest.java
+++ app/src/main/java/com/fmt/cheaptrip/ws/request/CustomStringRequest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,25 +26,38 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 /**
- * http://www.itworld.com/article/2702452/development/how-to-send-a-post-request-with-google-volley-on-android.html
+ *
  */
 public class CustomStringRequest extends Request<String> {
 
-    private Listener mListener = null;
-    private Map<String, String> params = new HashMap<String, String>();;
 
+    private Listener mListener = null;
+    private Map<String, String> params = new HashMap<String, String>();
+
+    /**
+     * R
+     *
+     * @param method
+     * @param url
+     * @param mListener
+     * @param mErrorListener
+     */
     public CustomStringRequest(int method, String url, Listener mListener, ErrorListener mErrorListener) {
         super(method, url, mErrorListener);
         this.mListener = mListener;
     }
 
+    /**
+     * @param method
+     * @param url
+     */
     public CustomStringRequest(int method, String url) {
         this(method, url, null, null);
     }
 
     @Override
     protected Response<String> parseNetworkResponse(NetworkResponse response) {
-        String parsed;
+        String parsed = "";
         try {
             parsed = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
         } catch (UnsupportedEncodingException e) {
@@ -54,7 +68,7 @@ public class CustomStringRequest extends Request<String> {
 
     @Override
     protected void deliverResponse(String response) {
-        if(this.mListener != null) {
+        if (this.mListener != null) {
             mListener.onResponse(response);
         }
     }
@@ -70,7 +84,9 @@ public class CustomStringRequest extends Request<String> {
         return params;
     }
 
-    //Util function
+    /**
+     * Util function
+     */
     public void addParam(String key, String value) {
         getParams().put(key, value);
     }
