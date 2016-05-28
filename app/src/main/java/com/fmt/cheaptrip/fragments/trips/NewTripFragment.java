17a31c7431442c9ce70eeafca062f676a2bfb0d7
Tiago Santos <tcmsbd@gmail.com>
2016-05-28 15:23:17 +0100

    adding listeners

--- app/src/main/java/com/fmt/cheaptrip/fragments/trips/NewTripFragment.java
+++ app/src/main/java/com/fmt/cheaptrip/fragments/trips/NewTripFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ import com.fmt.cheaptrip.webservices.TripWSInvoker;
 import com.fmt.cheaptrip.webservices.response.WSResponseListener;
 import com.fmt.cheaptrip.webservices.response.WSResponseObject;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -30,16 +32,18 @@ public class NewTripFragment extends Fragment {
     public static final String TAG = "NEW_TRIPS_FRAGMENT_TAG";
 
     private final String PORTUGAL_CURRENCY = "€";
-    private final String DEFAULT_TRIP_TOLERANTE = "15";
+    private final String MINUTES = "min"; //AHAHAHAHHAHAH
 
-    private int tripPrice = 15;
-
+    private double tripTolerance = 15;
+    private double tripPrice = 15;
 
     // Location section views
     private TextView new_trip_fragment_origin_city_tv;
     private TextView new_trip_fragment_destiny_city_tv;
 
     // Date section views
+    private DatePicker new_trip_fragment_date_dp;
+
 
     // Price section views
     private FloatingActionButton new_trip_fragment_price_fb_less;
@@ -63,18 +67,17 @@ public class NewTripFragment extends Fragment {
     // Rules views
     private CheckBox new_trip_fragment_rules_cb_value;
 
-    private FloatingActionButton btnRegisterTrip;
+    // Register "button"
+    private TextView registerTripTextView;
 
 
     public NewTripFragment() {
-
+        //Default constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_trip, container, false);
-
 
         // Location section views
         new_trip_fragment_origin_city_tv = (TextView) view.findViewById(R.id.new_trip_fragment_origin_city_tv);
@@ -83,6 +86,7 @@ public class NewTripFragment extends Fragment {
         new_trip_fragment_destiny_city_tv.setText(getArguments().get("addressDestiny").toString());
 
         // Date section views
+        new_trip_fragment_date_dp = (DatePicker) view.findViewById(R.id.new_trip_fragment_date_dp);
 
         // Price section views
         new_trip_fragment_price_fb_less = (FloatingActionButton) view.findViewById(R.id.new_trip_fragment_price_fb_less);
@@ -112,8 +116,23 @@ public class NewTripFragment extends Fragment {
 
         // Time tolerance views
         new_trip_fragment_tolerance_fb_less = (FloatingActionButton) view.findViewById(R.id.new_trip_fragment_tolerance_fb_less);
+        new_trip_fragment_tolerance_fb_less.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new_trip_fragment_tolerance_tv_value.setText(String.valueOf(--tripTolerance) + " " + MINUTES);
+            }
+        });
+
         new_trip_fragment_tolerance_tv_value = (TextView) view.findViewById(R.id.new_trip_fragment_tolerance_tv_value);
+        new_trip_fragment_tolerance_tv_value.setText(String.valueOf(tripTolerance) + " " + MINUTES);
+
         new_trip_fragment_tolerance_fb_more = (FloatingActionButton) view.findViewById(R.id.new_trip_fragment_tolerance_fb_more);
+        new_trip_fragment_tolerance_fb_more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new_trip_fragment_tolerance_tv_value.setText(String.valueOf(++tripTolerance) + " " + MINUTES);
+            }
+        });
 
         // Obeservation vies
         new_trip_fragment_observation_et_value = (EditText) view.findViewById(R.id.new_trip_fragment_observation_et_value);
@@ -121,9 +140,9 @@ public class NewTripFragment extends Fragment {
         // Rules views
         new_trip_fragment_rules_cb_value = (CheckBox) view.findViewById(R.id.new_trip_fragment_rules_cb_value);
 
-        btnRegisterTrip = (FloatingActionButton) view.findViewById(R.id.btnRegisterTrip);
+        registerTripTextView = (TextView) view.findViewById(R.id.btnRegisterTrip);
 
-        btnRegisterTrip.setOnClickListener(new View.OnClickListener() {
+        registerTripTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 registerTrip(view);
@@ -138,14 +157,24 @@ public class NewTripFragment extends Fragment {
         final Trip trip = new Trip();
         trip.setDriverId(1);
         trip.setVehicleId(1);
-        trip.setStartCity("Lisboa");
-        trip.setEndCity("Beja");
+        trip.setStartCity(new_trip_fragment_origin_city_tv.getText().toString());
+        trip.setEndCity(new_trip_fragment_destiny_city_tv.getText().toString());
         trip.setStartPoint("1");
         trip.setEndPoint("2");
-        trip.setTripDate(new Date());
-        trip.setPrice(20.0d);
-        trip.setObservations("Obs");
-        trip.setBaggageSize("1");
+
+        Calendar calendar;
+        {
+            int day = new_trip_fragment_date_dp.getDayOfMonth();
+            int month = new_trip_fragment_date_dp.getMonth();
+            int year = new_trip_fragment_date_dp.getYear();
+            calendar = Calendar.getInstance();
+            calendar.set(year, month, day);
+        }
+
+        trip.setTripDate(calendar.getTime());
+        trip.setPrice(tripPrice);
+        trip.setObservations(new_trip_fragment_observation_et_value.getText().toString());
+        trip.setBaggageSize("");
         trip.setDelayTolerance(15);
 
         if (trip.getDriverId() == null || trip.getVehicleId() == null ||
@@ -160,20 +189,27 @@ public class NewTripFragment extends Fragment {
 
         } else {
 
+            if (new_trip_fragment_rules_cb_value.isChecked()) {
 
-            TripWSInvoker.registerTrip(getActivity().getApplicationContext(), trip, new WSResponseListener() {
-                @Override
-                public void onResponse(WSResponseObject response) {
-                    if (response.getSuccess().equalsIgnoreCase("true")) {
-                        Toast.makeText(getActivity().getApplicationContext(), "Trip Registered", Toast.LENGTH_SHORT).show();
+                TripWSInvoker.registerTrip(getActivity().getApplicationContext(), trip, new WSResponseListener() {
+                    @Override
+                    public void onResponse(WSResponseObject response) {
+                        if (response.getSuccess().equalsIgnoreCase("true")) {
+                            Toast.makeText(getActivity().getApplicationContext(), "Trip Registered", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
 
-                @Override
-                public void onError(VolleyError error) {
+                    @Override
+                    public void onError(VolleyError error) {
 
-                }
-            });
+                    }
+                });
+            } else {
+
+                Toast.makeText(getActivity(), "You have to agree with the rules", Toast.LENGTH_LONG);
+            }
         }
     }
+
+
 }
